c8ca38fd075d6483260a7cadd6037a52733a3db4
yuanwm <yuanwm@asiainfo.com>
2016-07-07 11:45:24 +0800

    [new feature]modify for jd hadoop.  Run image with COMMON ARGS, e.g. docker run -d -p 8000:8000 ****** --name hadoop hadoop /start.sh jd 	modified:   config/hosts 	modified:   config/krb5.conf 	new file:   config/krb5.conf.jd 	modified:   config/start.sh

--- config/start.sh
+++ config/start.sh
@@ -2,11 +2,21 @@
 echo "modify /etc/hosts"
 cat ./config/hosts  >> /etc/hosts
 
+echo "parametre1="$1
+
 echo "kinit"
 date
-kinit ocdp/h-4lcf6qdz@ASIAINFO.COM <<!!
-asiainfo
+if [ "$1" = "jd" ];then
+	cp ./config/krb5.conf.jd /etc/krb5.conf
+	kinit ocdp-OCDPforLDP@ASIAINFO.COM <<!!
+	asiainfo
 !!
+else
+	kinit ocdp/h-4lcf6qdz@ASIAINFO.COM <<!!
+	asiainfo
+!!
+fi
+
 
 echo "start main"
 date
